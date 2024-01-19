c85bf19ad2da8bb79d9be39b30ad8da5c17f7267
akumarmail99 <akumarmail99@gmail.com>
2024-01-19 18:58:07 +0530

    Design Patterns: Singleton

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# javascript-design-patterns
+# Javascript Design Patterns
+
+## Creational Design Patterns
+
+- Singleton Design Pattern:-
+- - The Singleton pattern limits the number of instances of a particular object to just one.
+- - Singleton is useful in a situations where system-side actions needs to be coordinated from a central place.
---
+++ creational-design-patterns/singleton.js
@@ -0,0 +1,25 @@
+const Singleton = (function(){
+    let instance;
+
+    function createInstanse() {
+        const newInstanse = new Object("new Instanse created.");
+        return newInstanse;
+    }
+
+    function getInstanse() {
+        if (!instance) {
+            instance = createInstanse();
+        }
+        return instance;
+    }
+
+    return {
+        getInstanse
+    }
+})();
+
+
+const instanse_one = Singleton.getInstanse();
+const instance_two = Singleton.getInstanse();
+
+console.log(instanse_one === instance_two);
